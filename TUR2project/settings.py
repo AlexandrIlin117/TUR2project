984013f808888e61024f3a633ba6235e5e3fd557
AlexandrIlin117 <MaCIsBlocked@gmail.com>
2022-11-04 21:03:22 +0300

    REST API workout V1.1: - установлен psycopg2 для работы с PostgrasQL; - установлен пакет djangorestframework; - установлен пакет markdown; - установлен пакет django-filter; - настроено подключение к PostgrasQL используя переменные окружения; - проделаны миграции; - создан администратор;

--- TUR2project/settings.py
+++ TUR2project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TUR2project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pereval7',
+        'USER': config('FSTR_LOGIN'),
+        'PASSWORD': config('FSTR_PASS'),
+        'HOST': config('FSTR_DB_HOST'),
+        'PORT': config('FSTR_DB_PORT'),
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
